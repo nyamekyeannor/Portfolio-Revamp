65cf35a01f31cb68b9d78b78cfb0ffb6db1b75e9
Nyamekye Annor <nyamekyeannor@gmail.com>
2024-06-18 23:27:10 -0400

    created hamburger toggle for work page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let lastScrollTop = 0;
+  const navbar = document.getElementById("navbar");
   const navToggle = document.getElementById("nav-toggle");
   const mobileMenu = document.getElementById("mobile-menu");
+
+  window.addEventListener("scroll", function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+      // Scroll Down
+      navbar.style.top = "-60px"; // Adjust this value based on your navbar height
+    } else {
+      // Scroll Up
+      navbar.style.top = "0";
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+  });
 
   navToggle.addEventListener("click", function () {
     mobileMenu.classList.toggle("active");
   });
-});
-
-let lastScrollTop = 0;
-const navbar = document.getElementById("navbar");
-let navbarHeight = navbar.offsetHeight;
-
-window.addEventListener("scroll", function () {
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (scrollTop > lastScrollTop) {
-    // Scroll down
-    navbar.style.top = `-${navbarHeight}px`;
-  } else {
-    // Scroll up
-    navbar.style.top = "0";
-  }
-
-  lastScrollTop = scrollTop;
 });
